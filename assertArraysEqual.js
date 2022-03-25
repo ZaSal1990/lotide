0bc61cd29586af6eb0c3f7d17c1e57beb927f55a
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-03-25 17:01:32 -0600

    assertion of array comparison

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+
+
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let loopOne = 0; loopOne < arrayOne.length; loopOne++) {
+      for (let loopTwo = 0; loopTwo < arrayTwo.length; loopTwo++) {
+        if (arrayOne[loopOne] === arrayTwo[loopTwo]) {
+          return `Assertion Passed: ${arrayOne} === ${arrayTwo}`;
+        } else if (arrayOne[loopOne] !== arrayTwo[loopTwo]) {
+          return `Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
+        }
+      }
+    }
+  }
+};
+
+
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
