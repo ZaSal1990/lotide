c7f7477a4fbe033244d5e0b1a95e3144e9403784
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-03-25 15:04:28 -0600

    array elements comparison

---
+++ eqArray.js
@@ -0,0 +1,26 @@
+
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let loopOne = 0; loopOne < arrayOne.length; loopOne++) {
+      for (let loopTwo = 0; loopTwo < arrayTwo.length; loopTwo++) {
+        if (arrayOne[loopOne] === arrayTwo[loopTwo]) {
+          return true;
+        } else if (arrayOne[loopOne] !== arrayTwo[loopTwo]) {
+          return false;
+        }
+      }
+    }
+  }
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
