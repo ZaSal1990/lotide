daf3922978b1994d6f3d8885366ffad53e1af306
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-03-28 09:40:23 -0600

    CountOnly - prgm counting data on one list specified on the second list

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  
+ let countedNames = allItems.reduce(function(allNames, name) {
+    if (name in allNames) {
+      allNames[name]++;
+    } else {
+      allNames[name] = 1;
+    }
+    return allNames;
+  }, {});
+  
+
+  let newObject = itemsToCount;
+  for (const item in newObject) {
+    if (newObject[item] === false) { //value of key(name)
+      delete newObject[item];
+    } else if (newObject[item] === true) {
+      newObject[item] = countedNames[item]; //replacing the value with the value of same key in countedNames object
+    }
+  }
+  return newObject;
+  
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+console.log(result1);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
--- password.js
+++ password.js
@@ -3,4 +3,26 @@ Every "a" in the string should be replaced with a "4".
 Every "e" in the string should be replaced with a "3".
 Every "o" (oh) in the string should be replaced with a "0" (zero).
 Every "l" (el) in the string should be replaced with a "1" (one).
+*/
 
+
+const passwordObfuscate = function(userInput) {
+  let password = "";
+  for (let char = 0; char < userInput.length; char++) {
+    if (userInput[char] === "a")  {
+      password += '4';
+    } else if (userInput[char] === "e")  {
+      password += '3';
+    } else if (userInput[char] === "o")  {
+      password += '0';
+    } else if (userInput[char] === "l")  {
+      password += '1';
+    } else {
+      password += userInput[char];
+    }
+  }
+  return password;
+};
+
+let userInput = process.argv[2];
+console.log(passwordObfuscate(userInput));
