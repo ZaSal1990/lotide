283238f274da42f3f577bafc2da8ce3ebdf24be1
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-03-29 18:32:59 -0600

    counting letters with two different codes

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
 /*const countLetters = function(string) {
  let newString = string.split('');
   //console.log(newString);
@@ -18,7 +11,7 @@ const assertEqual = function(actual, expected) {
 */
 
 
-const countLetters = function(string) {
+/*const countLetters = function(string) { 
   var counts = {};
   let char; 
   for (index = 0; index < string.length; index++) {
@@ -26,11 +19,16 @@ const countLetters = function(string) {
     counts[char] = counts[char] ? counts[char] + 1 : 1; // counting and assigning in the same line -- assign property value = +1 if exsists in the object
   }
   return counts;
-}
+}*/
 
-
-
-
+const countLetters = function(string) {
+  let counts = {};
+  for (const char of string) {
+    counts[char] = (counts[char] || 0) + 1; // assigining each character as key inside counts object LHS, ORing with 0 to see if the each character is unique in the objcet, if not, add 1 to the count
+  }
+  return counts;
+};
 
 
 console.log(countLetters('LHL'));
+
