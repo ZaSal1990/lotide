859faf25cad0ac2622cc333883a29433a3f0efd5
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-03-30 18:43:56 -0600

    object assertion

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(object, value) {
+let result;
+
+  for (let values in object)  {
+    if (object[values] === value)
+      result = values; // returning key to result if the value matches
+}
+return result;
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,34 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  
 
-function letterPositions(string) {
+  for (let index = 0; index < sentence.length; index++) {
+    results[sentence[index]] = (results[sentence[index]] || 0) + 1;
+    for (indexTwo = 0; indexTwo < results[sentence[index]]; results[sentence[index]]++)
+    {
+      if (typeof results[sentence[index]] === 'number')
+      {
+        let newArray = [];
+        results[sentence[index]] = newArray.push(indexTwo);
+      }
+      else if (typeof results[sentence[index]] === 'object')
+      {
+        results[sentence[index]] = newArray.push(indexTwo);
+      }
+    }
 
-}
+    //results[sentence[index]] = results[sentence[index]] > 1 ? createDataSet(index, Object.values(results[sentence[index]])) : index;
+    
+   
+    
+  }
+ 
+  return results;
+  
+};
 
+const createDataSet = function(index, value) {
+return value;
+};
 
 console.log(letterPositions("hello"));
