61b069a14d3d95b765a80f45a3e57e3dd4b63774
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-12 17:15:32 -0600

    headTest test assertion with chai & mocha

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,14 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+
+
+
+// describe("Palindromes", function() {
+//   it("should return true if a word is a palindrome", function() {
+//     const word = "abba";
+//     const result = isPalindrome(word);
+//     assert.isTrue(true, result);
+//   });
+
+assertArraysEqual([1, 2, 3], [1, 1, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
