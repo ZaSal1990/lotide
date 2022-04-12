e4e9edf342549d6b0cb94b77b84c0348e60e75c3
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-12 06:43:16 -0600

    exporting fnsw while tests reside on local test drive inside test dir in lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
 //prgm to assert/compare items in one array to their counter parts in teh otehr array
-
+const eqArrays = require('./eqArray');
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (arrayOne.length === arrayTwo.length) {
@@ -12,19 +12,20 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
 };
 
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  let result;
-  if (arrayOne.length === arrayTwo.length) {
-    for (let index = 0; index < arrayOne.length; index++) {
-      if (arrayOne[index] !== arrayTwo[index])  {
-        result = false;
-        break;
-      } else if (arrayOne[index] === arrayTwo[index])
-        result = true;
-    }
-  }
-  return result;
-};
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   let result;
+//   if (arrayOne.length === arrayTwo.length) {
+//     for (let index = 0; index < arrayOne.length; index++) {
+//       if (arrayOne[index] !== arrayTwo[index])  {
+//         result = false;
+//         break;
+//       } else if (arrayOne[index] === arrayTwo[index])
+//         result = true;
+//     }
+//   }
+//   return result;
+// };
   
-assertArraysEqual([1, 2, 3], [1, 1, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 1, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
