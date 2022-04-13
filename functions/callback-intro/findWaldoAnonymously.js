523a35364c4ce6b79e61be89732b0e1c74978b2b
ZaSal1990 <76197976+ZaSal1990@users.noreply.github.com>
2022-04-13 05:30:14 -0600

    Delete functions directory

--- functions/callback-intro/findWaldoAnonymously.js
+++
@@ -1,9 +0,0 @@
-// The second argument/parameter is expected to be a (callback) function
-const findWaldo = function(names, found) {
-  let foundWaldo = names.forEach(element => {
-    if (element === 'Waldo')
-      found(names.indexOf(element)); }); //passing index of Waldo as arguement
-};
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {console.log("Found Waldo at " + index);
-}); // inline anonymous fns
--- functions/map_builtin/mapTest.js
+++
@@ -1,5 +0,0 @@
-const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
-
-// write code here to use map
-// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
-console.log(lighthouses.map(element => {  return element.length;  }));
