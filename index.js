2ab32d11c0dccf094feb1dde74e67d3d9be9511e
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-13 09:56:36 -0600

    index file with export module configured with ES6 shorthand

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ const map = require('./map');
 const letterPositions = require('./letterPositions');
 const flatten = require('./flatten');
 const findKeyByValue = require('./findKeyByValue');
-const eqObjects = require('./eqObject');
-const eqArrays = require('./eqArray');
+const eqObjects = require('./eqObject');//
+const eqArrays = require('./eqArray');//
 const countOnly = require('./countOnly');
 const countLetters = require('./countLetters');
-const assertObjectEqual = require('./assertObjectsEqual');
+const assertObjectEqual = require('./assertObjectsEqual');//
 const assertEqual = require('./assertEqual');
 const findKey = require('./findKey');
 const assertArraysEqual = require('./assertArraysEqual');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  without: without,
-  takeUntil: takeUntil,
-  map: map,
-  letterPositions: letterPositions,
-  flatten: flatten,
-  findKeyByValue: findKeyByValue,
-  eqObjects: eqObjects,
-  eqArrays: eqArrays,
-  countOnly: countOnly,
-  countLetters: countLetters,
-  assertObjectEqual: assertObjectEqual,
-  assertEqual: assertEqual,
-  findKey: findKey,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  flatten,
+  findKeyByValue,
+  eqObjects,
+  eqArrays,
+  countOnly,
+  countLetters,
+  assertObjectEqual,
+  assertEqual,
+  findKey,
+  assertArraysEqual
 };
