17072bbc9c50d6d91d70d31915c6e1ac80f77422
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-13 18:49:28 -0600

    after fixing map fns as per evaluation

--- map.js
+++ map.js
@@ -2,12 +2,10 @@
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    if (item.length > 5)
-      results.push(callback(item));
+    results.push(callback(item));
   }
   return results;
 };
-
 
 
 module.exports = map;
