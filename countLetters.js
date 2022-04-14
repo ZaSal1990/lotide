7c48c01e2fa2caf798eeedc03a00720f915d1a2a
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-13 18:32:10 -0600

    after fixing fns to not count space as per evaluation result

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,6 @@
 
 const countLetters = function(string) {
+  string = string.replaceAll(' ','');
   let counts = {};
   for (const char of string) {
     counts[char] = (counts[char] || 0) + 1; // assigining each character as key inside counts object LHS, ORing with 0 to see if the each character is unique in the objcet, if not, add 1 to the count
