e6676c360435653cd921366035982fcbc261cc0f
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-14 18:03:42 -0600

    fix without, catering removal of multiple items

--- without.js
+++ without.js
@@ -1,18 +1,9 @@
 
 
-
-const without = function (sourceArray, itemsToRemove) {
-  for (let loopOne = 0; loopOne < itemsToRemove.length; loopOne++) { //2
-    for (let loopTwo = 0; loopTwo < sourceArray.length; loopTwo++) { //3
-      if (sourceArray[loopTwo] === itemsToRemove[loopOne])
-      {
-        sourceArray.splice(loopTwo,1);
-      };
-    }
-  }
-return sourceArray;
+const without = function(sourceArray, itemsToRemove) {
+  const filteredItems = sourceArray.filter(item => !itemsToRemove.includes(item));
+  return filteredItems;
 };
-
 
 
 module.exports = without;
